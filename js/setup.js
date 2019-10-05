7ec73fad8a7fb707b5941fbeec9ac4407c6f8f8e
Антон Куринной <adra1985@gmail.com>
2019-10-05 11:05:20 +0300

    changes made according to mentor's comments

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,59 @@
 'use strict';
 
 // данные о волшебниках
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_COUNT = 4;
+
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var WIZARD_COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var WIZARD_WIZARD_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 
 // устанавливаем видимость окон
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+// функция удаления дпо. класса
+var deleteClass = function (selectorName, className) {
+  var userDialog = document.querySelector(selectorName);
+  return userDialog.classList.remove(className);
+};
+
+// вызываем функцию и удаляем класс у окна настроек персонажа
+deleteClass('.setup', 'hidden');
+// вызываем функцию и удаляем класс у окна похожих персонажей
+deleteClass('.setup-similar', 'hidden');
+
 
 // Эта маленькая строка кода будет обращаться к случайному элементу в массиве, генерируя случайное число с плавающей точкой от нуля до длины массива и округляя его до ближайшего целого числа
 var randomElement = function (array) {
@@ -20,7 +63,7 @@ var randomElement = function (array) {
 // функция создания объекта с рандомными параметрами волшебников
 var randomGenerateWizard = function () {
   var wizard = {
-    name: randomElement(WIZARD_NAMES) + '\n' + randomElement(WIZARD_SURNAMES),
+    name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_SURNAMES),
     coatColor: randomElement(WIZARD_COAT_COLORS),
     eyesColor: randomElement(WIZARD_WIZARD_EYES_COLORS)
   };
@@ -29,6 +72,7 @@ var randomGenerateWizard = function () {
 
 // находим блоки с нужными классами в шаблоне и записываем в них параметры из функции randomGenerateWizard
 var renderWizard = function (wizard) {
+
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -38,12 +82,24 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
+// перебор массива волшебников
+var getWizardsArray = function (wizardsCount) {
+  var wizardsArray = [];
+  for (var i = 0; i < wizardsCount; i++) {
+    wizardsArray.push(randomGenerateWizard());
+  }
+  return wizardsArray;
+};
 
-// выводить не больше 4 елементов
-for (var i = 0; i < 4; i++) {
-  fragment.appendChild(renderWizard(randomGenerateWizard()));
-}
-similarListElement.appendChild(fragment);
+// функция отрисовки похожих волшебников
+var createSimilarWizard = function (wizardSimilar) {
+  var fragment = document.createDocumentFragment();
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  for (var i = 0; i < wizardSimilar.length; i++) {
+    fragment.appendChild(renderWizard(wizardSimilar[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+var wizardsCountSimilar = getWizardsArray(WIZARD_COUNT);
+createSimilarWizard(wizardsCountSimilar);
